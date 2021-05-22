7a9ecad1eb19a3ec0b527ae8d81761a1495c9a29
xioxin <5716100+xioxin@users.noreply.github.com>
2021-05-22 20:58:30 +0800

    Update EhDetailPageTagColor.user.js
    修改图标为标签颜色色块,用于快速分辨页面包含的标签

--- DetailPageTagColor/EhDetailPageTagColor.user.js
+++ DetailPageTagColor/EhDetailPageTagColor.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         eh详情页标签颜色
-// @namespace    com.xioxin.detailPageTagColor
-// @version      0.1
-// @description  eh为详情页标签增加颜色,先访问一次我的标签才可生效.
+// @namespace    com.xioxin.tag-color
+// @version      0.2
+// @description  eh为详情页标签增加颜色,修改网页图标为标签颜色
 // @author       xioxin
 // @include     *://exhentai.org/g/*
 // @include     *://e-hentai.org/g/*
@@ -40,9 +40,34 @@ async function dyeing() {
         }
         `
     });
-    console.log("css", css);
     GM_addStyle(css);
+    setTimeout(colorIcon, 10);
 }
+
+function colorIcon() {
+    const tagColors = [...document.querySelectorAll("#taglist td>div")].map(e => (/(rgb\(.*?\))/ig.exec(getComputedStyle(e).backgroundImage)||[])[0]).filter(v => v);
+    const colors = tagColors;
+    colors.sort();
+    if(!colors.length) return;
+    const canvas = document.createElement('canvas');
+    canvas.width = canvas.height = 128;
+    let ctx = canvas.getContext("2d");
+    colors.forEach((c, i) => {
+        ctx.fillStyle = ctx.strokeStyle = c;
+        ctx.fillRect(canvas.width / colors.length * i, 0, canvas.width / colors.length, canvas.height);
+    });
+    const link = canvas.toDataURL('image/png');
+    let favicon = document.querySelector('link[rel="icon"]');
+    if (favicon !== null) {
+        favicon.href = link;
+    } else {
+        favicon = document.createElement("link");
+        favicon.rel = "icon";
+        favicon.href = link;
+        document.head.appendChild(favicon);
+    }
+}
+
 
 if(window.location.pathname == "/mytags")saveMyTagData();
 if(window.location.pathname.slice(0, 3) == '/g/')dyeing();
