2ff8d43fe4bc77fd13d7f3636591fda5aa3a1e96
枫谷剑仙 <mapaler@163.com>
2023-09-28 22:16:10 +0800

    1.解决一些不显示的新URL地址； 2.显示文件名； 3.更新部分代码风格

--- Thumbnail/ETTHelper-Thumbnail.user.js
+++ Thumbnail/ETTHelper-Thumbnail.user.js
@@ -7,12 +7,12 @@
 // @include     *://exhentai.org/g/*
 // @include     *://e-hentai.org/g/*
 // @include     *://e-hentai.org/lofi/g/*
-// @include     *://upload.e-hentai.org/managegallery*
-// @include     *://upld.e-hentai.org/managegallery*
-// @include     *://exhentai.org/upload/managegallery*
-// @include     *://exhentai.org/upld/managegallery*
-// @resource    ui-style https://github.com/EhTagTranslation/UserScripts/raw/master/Thumbnail/ETTHelper-Thumbnail.ui.css?v=3.0.3
-// @version     3.0.4
+// @include     *://upl*d.e-hentai.org/managegallery*
+// @include     *://upl*d.e-hentai.org/upl*d/managegallery*
+// @include     *://exhentai.org/upl*d/managegallery*
+// @include     *://upl*d.exhentai.org/upl*d/managegallery*
+// @resource    ui-style https://github.com/EhTagTranslation/UserScripts/raw/master/Thumbnail/ETTHelper-Thumbnail.ui.css?v=3.1
+// @version     3.1.0
 // @grant       GM_getResourceText
 // @grant       GM_addStyle
 // @grant       GM_setClipboard
@@ -23,81 +23,102 @@
 // @supportURL  https://github.com/EhTagTranslation/UserScripts/issues
 // ==/UserScript==
 
-let styleText = GM_getResourceText("ui-style");
-let thumbs;
-if (/^\/g\//.test(location.pathname)) //图像画廊
-{
-	thumbs = Array.from(document.querySelectorAll('#gdt>.gdtl'));
-}else if (/^\/lofi\//.test(location.pathname)) //手机版画廊
-{
-	thumbs = Array.from(document.querySelectorAll('#gh>.gi'));
-}else if (/managegallery/.test(location.pathname)) //画廊编辑
-{
-	thumbs = Array.from(document.querySelectorAll('#t>.nosel>div'));
-}
-const pageLink = thumbs[0]?.nodeName == 'A' ? thumbs[0] : thumbs[0]?.querySelector('a');
-if (pageLink) {
+const styleText = GM_getResourceText("ui-style");
+GM_addStyle(styleText);
+const { thumbs, thumbType } = (()=>{
+	let thumbs, thumbType;
+	if (/^\/g\//.test(location.pathname)) {//图像画廊
+		thumbs = Array.from(document.querySelectorAll('#gdt>.gdtl'));
+		thumbType = 'gallery';
+	}
+	else if (/^\/lofi\//.test(location.pathname)) {//手机版画廊
+		thumbs = Array.from(document.querySelectorAll('#gh>.gi'));
+		thumbType = 'lofi';
+	}
+	else if (/managegallery/.test(location.pathname)) {//画廊编辑
+		thumbs = Array.from(document.querySelectorAll('#t>.nosel>div'));
+		thumbType = 'upload';
+	}
+	return { thumbs, thumbType };
+})();
+
+if (thumbType === 'lofi') {
+	const pageLink = thumbs[0]?.nodeName == 'A' ? thumbs[0] : thumbs[0]?.querySelector('a');
 	const match = pageLink.pathname.match(/\-(\d+)$/);
 	const firstPage = parseInt(match[1],10);
-	styleText += `
-body{
-	counter-reset: page ${firstPage-1};
+	const styleText2 = `
+		body{
+			counter-reset: page ${firstPage-1};
+		}
+		.EWHT-ul::before{
+			counter-increment: page;
+			content: "P" counter(page) ": ";
+		}
+	`;
+	GM_addStyle(styleText2);
 }
-.EWHT-ul::before{
-	counter-increment: page;
-	content: "P" counter(page) ": ";
-}
-`;
-}
-GM_addStyle(styleText);
 
-thumbs.forEach(thumb=>{
-	thumb.appendChild(buildBtnList());
-});
-
-function getImgId(src)
-{
+const getImgId = (src) => {
 	const url = new URL(src);
 	if (url.searchParams.get('fileid'))
 	{
 		return url.searchParams.get('fileid');
 	}else
 	{
-		let RegRes = /(\w+)\-(\d+)\-(\d+)\-(\d+)\-(\w+)(?:_l|_250)\b/i.exec(src);
-		if (RegRes)
-		{
+		const RegRes = /(\w+)\-(\d+)\-(\d+)\-(\d+)\-(\w+)(?:_l|_250)\b/i.exec(src);
+		if (RegRes) {
 			return `${RegRes[1]}-${RegRes[2]}-${RegRes[3]}-${RegRes[4]}-${RegRes[5]}`;
-		}else
-		{
+		}
+		else {
 			return null;
 		}
 	}
 }
-function copyString(){
-	const type = parseInt(this.value, 10);
+const creat_li = (type) => {
+	const li = document.createElement("li");
+	li.className = "EWHT-li";
+	const btn = li.appendChild(document.createElement("button"));
+	btn.className = "EWHT-btn";
+	btn.dataset.type = type;
+	btn.onclick = copyString;
+	return li
+}
+const buildBtnList = () => {
+	const ul = document.createElement("ul");
+	ul.className = "EWHT-ul";
+	ul.append(
+		creat_li("纯"),
+		creat_li("图"),
+		creat_li("隐"),
+		creat_li("限")
+	);
+	return ul;
+}
+
+function copyString() {
+	const type = this.dataset.type;
 	const src_original = this.parentNode.parentNode.parentNode.querySelector('img').src;
 	const fileId = getImgId(src_original);
-	if (fileId == null)
-	{
+	if (fileId === null) {
 		alert('错误：\n未找到符合格式的图片 ID。');
+		return;
 	}
-	const src_out = `https://ehgt.org/${fileId.substr(0,2)}/${fileId.substr(2,2)}/${fileId}_l.jpg`
+	const src_out = `https://ehgt.org/${fileId.substring(0,2)}/${fileId.substring(2,4)}/${fileId}_l.jpg`
 	
 	let outstr,typeName;
-	switch(type)
-	{
-		case 1:{
-			outstr = `![图](${src_out})`;
+	switch(type) {
+		case "图":{
+			outstr = `![${type}](${src_out})`;
 			typeName = "MD 格式图片地址";
 			break;
 		}
-		case 2:{
-			outstr = `![图](# "${src_out}")`;
+		case "隐":{
+			outstr = `![${type}](# "${src_out}")`;
 			typeName = "R18 MD 格式图片地址";
 			break;
 		}
-		case 3:{
-			outstr = `![图](## "${src_out}")`;
+		case "限":{
+			outstr = `![${type}](## "${src_out}")`;
 			typeName = "R18G 限制级 MD 格式图片地址";
 			break;
 		}
@@ -110,27 +131,21 @@ function copyString(){
 
 	GM_setClipboard(outstr);
 	GM_notification(outstr, //显示的文本
-		"已复制到剪贴板 - " +　typeName, //标题
-		src_out //缩略图地址
-		);
+		`已复制到剪贴板 - ${typeName}`, //标题
+		src_original //显示原版地址，这样可以节省加载时间
+	);
 }
-function buildBtnList()
-{
-	const ul = document.createElement("ul");
-	ul.className = "EWHT-ul";
-	function creat_li(caption,value){
-		const li = document.createElement("li");
-		li.className = "EWHT-li";
-		const btn = li.appendChild(document.createElement("button"));
-		btn.className = "EWHT-btn";
-		btn.appendChild(document.createTextNode(caption));
-		btn.value = value;
-		btn.onclick = copyString;
-		return li
+
+thumbs.forEach(thumb=>{
+	//获取到图像
+	const img = thumb.querySelector("img");
+	//如果图像有title
+	if (img.title) {
+		const filename = document.createElement("div");
+		filename.className = "filename";
+		filename.textContent = img.title;
+		img.parentElement.insertBefore(filename, img.nextSibling?.nextSibling?.nextSibling);
 	}
-	ul.appendChild(creat_li("纯",0));
-	ul.appendChild(creat_li("图",1));
-	ul.appendChild(creat_li("隐",2));
-	ul.appendChild(creat_li("限",3));
-	return ul;
-}
+
+	thumb.appendChild(buildBtnList());
+});
